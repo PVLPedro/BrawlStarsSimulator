272b037824f709366723c5f6f2e3914d8dcdb418
Pedro Henrique Custódio Brito <pedropedroka130608@gmail.com>
2026-04-29 22:43:57 -0300

    Versão 0.2 - Finalização dos draws das caixas, compra das caixas e contabilização adequada de recursos e ciaxas

---
+++ src/scripts/brawlers.js
@@ -0,0 +1,51 @@
+const brawlersMap = [
+	// GD = Gadget; ST = Star Power; HC = Hypercharge;
+
+	// Starter / Inicial;
+	{ brawler: "shelly", rarity: "starter", situation: "unlocked"},
+
+	// Rare / Raro;
+	{ brawler: "nita", rarity: "rare", situation: "locked"},
+	{ brawler: "colt", rarity: "rare", situation: "locked"},
+	{ brawler: "bull", rarity: "rare", situation: "locked"},
+	{ brawler: "brock", rarity: "rare", situation: "locked"},
+	{ brawler: "elPrimo", rarity: "rare", situation: "locked"},
+	// ...
+	
+	// Super Rare / Super Raro;
+	{ brawler: "jessie", rarity: "superRare", situation: "locked"},
+	{ brawler: "dynamike", rarity: "superRare", situation: "locked"},
+	{ brawler: "tick", rarity: "superRare", situation: "locked"},
+	{ brawler: "8Bit", rarity: "superRare", situation: "locked"},
+	{ brawler: "rico", rarity: "superRare", situation: "locked"},
+	// ...
+	
+	// Epic / Épico;
+	{ brawler: "bo", rarity: "epic", situation: "locked"},
+	{ brawler: "emz", rarity: "epic", situation: "locked"},
+	{ brawler: "stu", rarity: "epic", situation: "locked"},
+	{ brawler: "piper", rarity: "epic", situation: "locked"},
+	{ brawler: "pam", rarity: "epic", situation: "locked"},
+	// ...
+	
+	// Mythic / Mítico;
+	{ brawler: "mortis", rarity: "mythic", situation: "locked"},
+	{ brawler: "tara", rarity: "mythic", situation: "locked"},
+	{ brawler: "eugenio", rarity: "mythic", situation: "locked"},
+	{ brawler: "max", rarity: "mythic", situation: "locked"},
+	{ brawler: "misterP", rarity: "mythic", situation: "locked"},
+	// ...
+	
+	// Legendary / Lendário;
+	{ brawler: "spike", rarity: "legendary", situation: "locked"},
+	{ brawler: "crow", rarity: "legendary", situation: "locked"},
+	{ brawler: "leon", rarity: "legendary", situation: "locked"},
+	{ brawler: "sandy", rarity: "legendary", situation: "locked"},
+	{ brawler: "amber", rarity: "legendary", situation: "locked"},
+	// ...
+	
+	// Ultra Legendary / Ultra Lendário;
+	{ brawler: "kaze", rarity: "ultraLegendary", situation: "locked"},
+	{ brawler: "sirius", rarity: "ultraLegendary", situation: "locked"},
+    // ...
+]
